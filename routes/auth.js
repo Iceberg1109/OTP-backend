69ea44be85a12edb7374d6c2f284830279dd73f7
iceberg1109 <smartdev951109@gmail.com>
2020-11-10 10:04:15 +0800

    check otp length

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* Check OTP */
 router.post("/otp", function (req, res, next) {
   let { otp } = req.body;
-  if (otp[otp.length - 1] !== "7") return res.json({ status: "success" });
+  if (otp.length === 6 && otp[otp.length - 1] !== "7")
+    return res.json({ status: "success" });
   return res.json({ status: "error" });
 });
 
